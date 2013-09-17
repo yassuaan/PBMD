bfbb4bf5bf69d03d8818cdcaeca6c4a7601829e6
yassuaan <yasumassan@gmail.com>
2013-09-17 12:55:01 +0900

    change bootstrap のコントローラ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :change_bootstrap
+
+  def change_bootstrap
+    if params[:stylevariety]
+      @changeboot = params[:stylevariety]
+      cookies[:bootstylesheet_favorite] = @changeboot
+    elsif cookies[:bootstylesheet_favorite]
+      @changeboot = cookies[:bootstylesheet_favorite]
+    else
+      @changeboot = "cerulean"
+    end
+    
+  end
+  
 end
